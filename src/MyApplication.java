a68fcc57484749070123ce7acd4194e5cf24fcc0
55 ОМ <230895@astanait.edu.kz>
2023-12-20 10:17:55 +0600

    2init

---
+++ src/MyApplication.java
@@ -0,0 +1,15 @@
+import models.Point;
+import models.Shape;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MyApplication {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("C:\\Users\\55 ОМ\\IdeaProjects\\assignments\\src\\source.txt");
+        Scanner scan=new Scanner(file);
+        Shape shape=new Shape();
+
+    }
+}
