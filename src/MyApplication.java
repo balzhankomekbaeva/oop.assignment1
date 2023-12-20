c291e3e5e68d8abfef993f03b6f5d5d6ed9d220c
55 ОМ <230895@astanait.edu.kz>
2023-12-20 17:01:54 +0600

    3init

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,5 +1,5 @@
-import models.Point;
-import models.Shape;
+import assignment1.Point;
+import assignment1.Shape;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,8 +8,20 @@ import java.util.Scanner;
 public class MyApplication {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("C:\\Users\\55 ОМ\\IdeaProjects\\assignments\\src\\source.txt");
-        Scanner scan=new Scanner(file);
-        Shape shape=new Shape();
 
+        Scanner sc = new Scanner(file);
+        Shape shape = new Shape();
+
+        while (sc.hasNext()) {
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+
+            Point point = new Point(x, y);
+            shape.addPoint(point);
+        }
+
+        System.out.println(shape.calculatePerimeter());
+        System.out.println(shape.getLongest());
+        System.out.println(shape.getAverage());
     }
 }
---
+++ src/assignment1/Shape.java
@@ -0,0 +1,48 @@
+package assignment1;
+import java.util.ArrayList;
+
+public class Shape {
+    private ArrayList<Point> points;
+
+    // Constructor to initialize a shape with an array of points
+    public Shape() {
+        points = new ArrayList<>();
+    }
+
+    public void addPoint(Point point) {
+        points.add(point);
+    }
+
+    // Calculate and return the perimeter of the shape
+    public double calculatePerimeter() {
+        double perimeter = 0;
+        for (int i = 0; i < points.size() - 1; i++) {
+            perimeter += points.get(i).distanceTo(points.get(i + 1));
+        }
+        perimeter += points.get(points.size() - 1).distanceTo(points.get(0)); // Closing the shape
+        return perimeter;
+    }
+
+    // Find and return the length of the longest side in the shape
+    public double getLongest() {
+        double longestSide = 0;
+        for (int i = 0; i < points.size() - 1; i++) {
+            double sideLength = points.get(i).distanceTo(points.get(i + 1));
+            if (sideLength > longestSide) {
+                longestSide = sideLength;
+            }
+        }
+        longestSide = Math.max(longestSide, points.get(points.size() - 1).distanceTo(points.get(0))); // Closing the shape
+        return longestSide;
+    }
+
+    // Calculate and return the average length of the sides in the shape
+    public double getAverage() {
+        double totalSideLength = 0;
+        for (int i = 0; i < points.size() - 1; i++) {
+            totalSideLength += points.get(i).distanceTo(points.get(i + 1));
+        }
+        totalSideLength += points.get(points.size() - 1).distanceTo(points.get(0)); // Closing the shape
+        return totalSideLength / points.size();
+    }
+}
